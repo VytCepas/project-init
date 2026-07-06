5db0d36eabfe3f73867063ab8c02f032c8d816ef
Vytautas Čepas <vyt.cepas.ve@gmail.com>
2026-07-06 22:02:42 +0300

    fix(PI-606): Add pre_edit_issue_guard to plugin hooks

--- do_commit.sh
+++ do_commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/projects/project_init
 git add -A
-git commit -m "fix(PI-606): Restore plugins directory and rename plugin metadata to .agents-plugin"
+git commit -m "fix(PI-606): Add pre_edit_issue_guard to plugin hooks"
 .agents/scripts/push_branch.sh
